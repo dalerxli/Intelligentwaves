6f8c3f74fc97152fc31e2281bb01e7bae4489e67
shawnxiaow1118 <wangxiaonku@gmail.com>
2017-08-03 20:54:49 -0400

    feat: create methods to compute mesh information such as pcted and pctnd

--- create_mesh.py
+++ create_mesh.py
@@ -22,6 +22,33 @@ class squares():
 			self.up = 0
 		else:
 			self.up = void_list[row][col+1]
+		self.edges = None
+		self.index = None
+
+	def set_cell_index(self, num):
+		self.index = num 
+
+	def get_cell_index(self):
+		return self.index
+
+	def set_edge(self, cell_to_edge):
+		self.edges = cell_to_edge
+		self.e1 = self.edges[0]
+		self.e2 = self.edges[1]
+		self.e3 = self.edges[2]
+		self.e4 = self.edges[3]
+
+	def is_edge(self, edge_num):
+		if self.edges != None:
+			if edge_num in self.edges:
+				return True 
+			else:
+				return False
+		else:
+			# print(self.edges)
+			raise('Please run set_edge(cell_to_edge) fisrt')
+
+
 
 
 
@@ -82,7 +109,7 @@ def cellToNodes(void_list):
 			unit = squares(void_list, row, col)
 			one_row.append(unit)
 		square_list.append(one_row)
-	print(square_list)
+	# print(square_list)
 
 
 	node_to_delete = []
@@ -90,7 +117,7 @@ def cellToNodes(void_list):
 	for row in range(x_sqrs):
 		for col in range(y_sqrs):
 			cell_num = row*y_sqrs + col
-			print(cell_num)
+			# print(cell_num)
 			if square_list[row][col].is_filled == 0:
 				# void mesh, delete center node first
 				node_to_delete.append(cell_to_node[cell_num][4])
@@ -130,7 +157,7 @@ def cellToNodes(void_list):
 				for i in range(9):
 					num_to_change = cell_to_node[cell_num][i] 
 					num_less_than_n2c = len([j for j in node_to_delete if j < num_to_change])
-					print(num_to_change, num_less_than_n2c)
+					# print(num_to_change, num_less_than_n2c)
 					unit.append(num_to_change - num_less_than_n2c)
 				truncate_cell_to_node.append(unit)
 	print(truncate_cell_to_node)
@@ -145,6 +172,8 @@ def edgeToCell(void_list):
 
 	countx = 0
 	county = 0
+	# Ncel2indx = [i+1 for i in range(x_sqrs)]
+	# Ncel2indy = [i+1 for i in range(y_sqrs)]
 	Ncel2indx = []
 	Ncel2indy = []
 	for xcell in range(x_sqrs):
@@ -155,7 +184,7 @@ def edgeToCell(void_list):
 			Ncel2indy.append(county)
 			if county >= y_sqrs:
 				county = 0
-	print(Ncel2indx[0])
+	# print(Ncel2indx)
 
 	edge_to_cell = []
 	for ncell in range(x_sqrs*y_sqrs):
@@ -195,7 +224,63 @@ def edgeToCell(void_list):
 			pair[1] += 1
 
 	print(edge_to_cell)
-	return edge_to_cell
+	_, cell_to_edge, tol_edge = cellToEdge(void_list)
+	print(cell_to_edge)
+	# create squares() to manipulate positions of mesh
+	square_list = []
+	cell_count = 1
+	for row in range(x_sqrs):
+		one_row = []
+		for col in range(y_sqrs):
+			cell_num = row*y_sqrs + col
+			unit = squares(void_list, row, col)
+			unit.set_edge(cell_to_edge[cell_num])
+			if unit.is_filled:
+				print(cell_count)
+				unit.set_cell_index(cell_count)
+				cell_count += 1
+			one_row.append(unit)
+		square_list.append(one_row)
+
+	# edge_to_delete = []
+
+	# for row in range(x_sqrs):
+	# 	for col in range(y_sqrs):
+	# 		cell_num = row*y_sqrs + col
+	# 		if square_list[row][col].left == 0:
+	# 			edge_to_delete.append()	
+
+	# search for edge to
+	edge_dict = {}
+	print(square_list[0][1].edges) 
+	print(tol_edge)
+	for edge in range(1,tol_edge):
+		for row in range(x_sqrs):
+			for col in range(y_sqrs):
+				cell_num = row*y_sqrs + col
+				# print(square)
+				if square_list[row][col].is_edge(edge):
+					print('!!!')
+					if edge not in edge_dict.keys():
+						edge_dict[edge] = [square_list[row][col].index]
+					else:
+						edge_dict[edge].append(square_list[row][col].index)
+	print(edge_dict)
+	# append -1 to those with only one value
+	for value in edge_dict.values():
+		if len(value) == 1:
+			value.append(-1)
+	print(edge_dict)
+	# change to list representation
+	truncate_edge_to_cell = []
+	for edge in range(1, tol_edge):
+		truncate_edge_to_cell.append(edge_dict[edge])
+	print(truncate_edge_to_cell)
+
+
+
+	return edge_to_cell, truncate_edge_to_cell
+
 
 def cellToEdge(void_list):
 	x_sqrs = len(void_list)
@@ -246,8 +331,55 @@ def cellToEdge(void_list):
 		else:
 			raise('error')
 	print(cell_to_edge)
-	pass
 
+	square_list = []
+	for row in range(x_sqrs):
+		one_row = []
+		for col in range(y_sqrs):
+			unit = squares(void_list, row, col)
+			one_row.append(unit)
+		square_list.append(one_row)
+
+	edge_to_delete = []
+
+	for row in range(x_sqrs):
+		for col in range(y_sqrs):
+			cell_num = row*y_sqrs + col
+			if square_list[row][col].is_filled == 0:
+				if square_list[row][col].left == 0:
+					edge_to_delete.append(cell_to_edge[cell_num][3])
+				if square_list[row][col].right == 0:
+					edge_to_delete.append(cell_to_edge[cell_num][1])
+				if square_list[row][col].up == 0:
+					edge_to_delete.append(cell_to_edge[cell_num][2])
+				if square_list[row][col].down == 0:
+					edge_to_delete.append(cell_to_edge[cell_num][0])
+				# print(cell_num)
+	edge_to_delete = list(set(edge_to_delete))
+	edge_to_delete.sort()
+
+	truncate_cell_to_edge = []
+	tol_edge = x_sqrs * (y_sqrs + 1) + y_sqrs * (x_sqrs + 1) - len(edge_to_delete)
+	for row in range(x_sqrs):
+		for col in range(y_sqrs):
+			cell_num = row*y_sqrs + col
+
+			if void_list[row][col] == 0:
+				truncate_cell_to_edge.append([0 for i in range(4)])
+			else:
+				unit = []
+				for i in range(4):
+					num_to_change = cell_to_edge[cell_num][i] 
+					num_less_than_n2c = len([j for j in edge_to_delete if j < num_to_change])
+					# print(num_to_change, num_less_than_n2c)
+					unit.append(num_to_change - num_less_than_n2c)
+				truncate_cell_to_edge.append(unit)
+
+	print(edge_to_delete)
+	print(truncate_cell_to_edge)
+
+	return cell_to_edge, truncate_cell_to_edge, tol_edge
+	
 def printMesh(void_list):
 	rows = len(void_list)
 	print(rows)
@@ -269,7 +401,7 @@ if __name__ == '__main__':
 	a = nodePosition(void_list, 3,3)
 	print((a))
 	print(cellToNodes(void_list))
-	# edgeToCell(void_list)
+	edgeToCell(void_list)
 	cellToEdge(void_list)
 	sq = squares(void_list, 2, 2)
 	print(sq.left, sq.right, sq.up, sq.down)
